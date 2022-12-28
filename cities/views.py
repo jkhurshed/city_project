68a24001e23d4c62c07793b3b29e01f5a75709eb
jkhurshed <119507081+jkhurshed@users.noreply.github.com>
2022-12-28 20:04:58 +0500

    first commit

---
+++ cities/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from cities.models import City
+
+__all__ = (
+    'home',
+)
+
+
+def home(request, pk=None):
+    if pk:
+        city = City.objects.filter(id=pk).first()
+
+        context = {'object': city}
+        return render(request, 'cities/detail.html', context)
+
+    qs = City.objects.all()
+    context = {'objects_list': qs}
+    return render(request, 'cities/home.html', context)
+
